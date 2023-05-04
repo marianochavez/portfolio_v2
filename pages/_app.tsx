14a4817089f8113bf53e4849809c941ddb08764c
marianochavez <amarianochavez@gmail.com>
2023-05-04 12:15:02 -0300

    vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/index.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 import { ChakraProvider } from "@chakra-ui/react";
 import { DataProvider } from "@/context/DataProvider";
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <DataProvider>
       <ChakraProvider theme={theme}>
         <Component {...pageProps} />
+        <Analytics />
       </ChakraProvider>
     </DataProvider>
   );
